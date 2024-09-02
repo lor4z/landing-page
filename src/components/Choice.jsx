fd7e5efd5ecb013b2d52c2927f8499278d7fe5e3
lor4z <evellynlorainebr@gmail.com>
2024-09-02 17:49:10 -0400

    Mudanças no texto de informações

--- src/components/Choice.jsx
+++ src/components/Choice.jsx
@@ -13,23 +13,29 @@ const inforTelenova = () => {
                 <img src={logoCompleta} alt='Logo Telenova' id='logo-tel'/>
             </div>
             <div className='text-tel'>
-                <ul>
-                    <li>Por que escolher a Telenova?</li>
+        
+                    <h2>Por que escolher a Telenova?</h2>
                     <p>Trabalhamos com soluções tecnológicas mas sem perder nosso principal diferencial humano.
                     A Telenova traz soluções inteligentes, recursos inovadores para o desempenho da sua empresa, de forma próxima, humana e com um atendimento personalizado.
                     Porque sabemos que um dos principais pilares de sucesso de qualquer marca é um atendimento primoroso e eficiente. 
                     E para nós, cliente satisfeito é nosso dever e resultado da nossa dedicação.
                     </p>
+                <ul>
                     <li>Por isso nossos principais diferenciais são:</li>
                 </ul>
-                <li>Tecnologia de ponta para soluções personalizadas</li>
-                <button className="button-tel">
-                    <img src={seta} alt="seta branca" className="seta-img"/>
-                </button>
-                <li>Compromisso com prazos e qualidade</li>
-                <button className="button-tel2">
-                    <img src={seta} alt="seta branca" className="seta-img"/>
-                </button>
+               
+               <div className='button-info'>
+                    <button className="button-tel">
+                        <img src={seta} alt="seta branca" className="seta-img"/>
+                    </button>
+                    <p>Tecnologia de ponta para soluções personalizadas</p>
+                </div>
+                <div className='button-info'>
+                    <button className="button-tel2">
+                        <img src={seta} alt="seta branca" className="seta-img"/>
+                    </button>
+                    <p>Compromisso com prazos e qualidade</p>
+                </div>
                 <p>em todo processo - do atendimento a entrega e manutenção</p>
             </div>
         </div>
